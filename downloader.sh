7c91a534687dfa109ab0a91482c2aff72b4b5200
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-04 22:49:46 +0530

    Time for making stuff

--- downloader.sh
+++ downloader.sh
@@ -5,3 +5,9 @@ sudo pacman -Sy git linux-headers
 echo "Downloading the driver for RTL8723BE network card"
 
 git clone https://github.com/lwfinger/rtlwifi_new 
+
+cd rtlwifi_new
+
+make -j$(nproc --all)
+
+sudo make install -j$(nproc --all)
