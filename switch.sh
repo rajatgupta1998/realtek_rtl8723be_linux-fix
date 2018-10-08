47b0eed250760f93a289a49db16c8f831e7b6180
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-08 19:15:29 +0530

    Set Antenna #2
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- switch.sh
+++ switch.sh
@@ -3,3 +3,11 @@
 sudo modprobe -rv rtl8723be
 
 echo "Removing the module..."
+
+sudo modprobe -v rtl8723be ant_sel=2
+
+echo "Set the Antenna #2"
+
+echo "Run this script after each reboot"
+
+echo "Done"
