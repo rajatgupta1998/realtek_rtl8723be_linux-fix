a311c63286b2a8ad82d5bb37eeda91baed921ee2
Rajat Gupta <rjtgupta84@gmail.com>
2018-10-08 10:01:39 +0530

    Reboot and switch.sh
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- downloader.sh
+++ downloader.sh
@@ -11,3 +11,14 @@ cd rtlwifi_new
 make -j$(nproc --all)
 
 sudo make install -j$(nproc --all)
+
+echo "Please manually reboot your system or press y to reboot right now."
+
+echo "Continue with switch.sh after reboot"
+
+read var
+if [ $var == "y" && $var == "Y" ]
+then
+sudo reboot now;
+else exit
+fi
